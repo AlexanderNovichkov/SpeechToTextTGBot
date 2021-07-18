027b5899b9f64cf553af015fc2475948d4f2fa72
alexander <a.novichkov01@gmail.com>
2021-07-18 15:59:42 +0300

    Format code with Black

--- main.py
+++ main.py
@@ -1,55 +1,86 @@
 from builtins import bytes
 
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import Updater, Dispatcher, CommandHandler, MessageHandler, Filters, CallbackContext, \
-    CallbackQueryHandler, PicklePersistence
+from telegram.ext import (
+    Updater,
+    Dispatcher,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    CallbackContext,
+    CallbackQueryHandler,
+    PicklePersistence,
+)
 from telegram.update import Update
 
-from yandexclients import SpeechRecognition, SpeechRecognitionException, IAMToken, Language
+from yandexclients import (
+    SpeechRecognition,
+    SpeechRecognitionException,
+    IAMToken,
+    Language,
+)
 
 
 class SpeechRecognitionTGBot:
     def __init__(self, telegram_token: str, speech_recognition: SpeechRecognition):
         self._speech_recognition = speech_recognition
 
-        persistence = PicklePersistence(filename='botdata.pickle')
+        persistence = PicklePersistence(filename="botdata.pickle")
         self._updater = Updater(token=telegram_token, persistence=persistence)
 
         dispatcher: Dispatcher = self._updater.dispatcher
 
-        dispatcher.add_handler(CommandHandler('start', self.__start))
-        dispatcher.add_handler(CommandHandler('language', self.__language))
+        dispatcher.add_handler(CommandHandler("start", self.__start))
+        dispatcher.add_handler(CommandHandler("language", self.__language))
         dispatcher.add_handler(CallbackQueryHandler(self.__keyboard_callback_handler))
-        dispatcher.add_handler(MessageHandler(Filters.audio | Filters.voice, self.__voice_or_audio_message))
+        dispatcher.add_handler(
+            MessageHandler(Filters.audio | Filters.voice, self.__voice_or_audio_message)
+        )
 
     def start(self):
         self._updater.start_polling()
         self._updater.idle()
 
     def __start(self, update: Update, context: CallbackContext):
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="Этот бот распознает текст голосовых сообщений и пишет его в ответ.\n\n"
-                                      "/language - выбор языка распознавания речи")
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Этот бот распознает текст голосовых сообщений и пишет его в ответ.\n\n"
+            "/language - выбор языка распознавания речи",
+        )
 
     def __language(self, update: Update, context: CallbackContext):
-        keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton(lang.name, callback_data=lang.code) for lang in self._speech_recognition.LANGUAGES]
-        ])
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text=f"Текущий язык для распознавания: {self.__get_current_chat_language(context).name}. \n"
-                                      "Выберите новый язык",
-                                 reply_markup=keyboard)
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(lang.name, callback_data=lang.code)
+                    for lang in self._speech_recognition.LANGUAGES
+                ]
+            ]
+        )
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Текущий язык для распознавания: "
+            f"{self.__get_current_chat_language(context).name}. \n"
+            "Выберите новый язык",
+            reply_markup=keyboard,
+        )
 
     def __keyboard_callback_handler(self, update: Update, context: CallbackContext):
         query = update.callback_query
         language_code = query.data
         try:
-            language = next(filter(lambda lang: lang.code == language_code, self._speech_recognition.LANGUAGES), None)
+            language = next(
+                filter(
+                    lambda lang: lang.code == language_code,
+                    self._speech_recognition.LANGUAGES,
+                ),
+                None,
+            )
         except:
-            query.edit_message_text(f'Ошибка при обработке клавиатуры')
+            query.edit_message_text(f"Ошибка при обработке клавиатуры")
             return
 
-        query.edit_message_text(f'Выбран язык распознавания: {language.name}')
+        query.edit_message_text(f"Выбран язык распознавания: {language.name}")
         self.__set_new_chat_language(language, context)
 
     def __voice_or_audio_message(self, update: Update, context: CallbackContext):
@@ -59,36 +90,46 @@ class SpeechRecognitionTGBot:
             speech: bytes = update.message.audio.get_file().download_as_bytearray()
 
         try:
-            text = self._speech_recognition(speech, self.__get_current_chat_language(context).code)
+            text = self._speech_recognition(
+                speech, self.__get_current_chat_language(context).code
+            )
             if len(text) == 0:
                 text = "Не получилось здесь распознать речь"
         except SpeechRecognitionException:
-            text = "Ошибка при распознавании. Возможно, данный формат не поддерживается, или длительность больше 30 секунд."
-        context.bot.send_message(reply_to_message_id=update.message.message_id, chat_id=update.effective_chat.id,
-                                 text=text)
+            text = (
+                "Ошибка при распознавании. "
+                "Возможно, данный формат не поддерживается, или длительность больше 30 секунд."
+            )
+        context.bot.send_message(
+            reply_to_message_id=update.message.message_id,
+            chat_id=update.effective_chat.id,
+            text=text,
+        )
 
     def __get_current_chat_language(self, context: CallbackContext) -> Language:
-        return context.chat_data.get('language', self._speech_recognition.LANGUAGES[0])
+        return context.chat_data.get("language", self._speech_recognition.LANGUAGES[0])
 
     def __set_new_chat_language(self, language: Language, context: CallbackContext):
-        context.chat_data['language'] = language
+        context.chat_data["language"] = language
 
 
 def read_token_from_file(file_path: str) -> str:
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         return f.read()
 
 
 def main():
-    telegram_token = read_token_from_file('data/telegram-token.txt')
-    yandex_oauth_token = read_token_from_file('data/yandex-oauth-token.txt')
-    yandex_folder_id = read_token_from_file('data/yandex-folder-id.txt')
+    telegram_token = read_token_from_file("data/telegram-token.txt")
+    yandex_oauth_token = read_token_from_file("data/yandex-oauth-token.txt")
+    yandex_folder_id = read_token_from_file("data/yandex-folder-id.txt")
 
-    speech_recognition = SpeechRecognition(IAMToken(oauth_token=yandex_oauth_token), yandex_folder_id)
+    speech_recognition = SpeechRecognition(
+        IAMToken(oauth_token=yandex_oauth_token), yandex_folder_id
+    )
 
     srtb = SpeechRecognitionTGBot(telegram_token, speech_recognition)
     srtb.start()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- yandexclients.py
+++ yandexclients.py
@@ -1,15 +1,17 @@
 import threading
 import time
-from collections import namedtuple
 from datetime import timedelta
 
 import requests
 
 
 class IAMToken:
-    def __init__(self, oauth_token: str, time_between_update: timedelta = timedelta(hours=1)):
+    def __init__(
+        self, oauth_token: str, time_between_update: timedelta = timedelta(hours=1)
+    ):
         self.__oauth_token = oauth_token
         self.__time_between_updates = time_between_update
+
         self.__iam_token = None
         self.__update_iam_token()
         self.__th = threading.Thread(target=self.__schedule_token_updating, daemon=True)
@@ -24,14 +26,14 @@ class IAMToken:
             self.__update_iam_token()
 
     def __update_iam_token(self):
-        url = 'https://iam.api.cloud.yandex.net/iam/v1/tokens'
-        params = {'yandexPassportOauthToken': self.__oauth_token}
+        url = "https://iam.api.cloud.yandex.net/iam/v1/tokens"
+        params = {"yandexPassportOauthToken": self.__oauth_token}
         try:
             response = requests.post(url, params=params)
         except requests.exceptions.RequestException:
             return
         if response.status_code == 200:
-            self.__iam_token = response.json()['iamToken']
+            self.__iam_token = response.json()["iamToken"]
 
 
 class SpeechRecognitionException(Exception):
@@ -45,22 +47,23 @@ class Language:
 
 
 class SpeechRecognition:
-    __BASE_URL = 'https://stt.api.cloud.yandex.net/speech/v1/stt:recognize'
+    __BASE_URL = "https://stt.api.cloud.yandex.net/speech/v1/stt:recognize"
 
-    LANGUAGES = [Language('Russian', 'ru-RU'), Language('English', 'en-US')]
+    LANGUAGES = [Language("Russian", "ru-RU"), Language("English", "en-US")]
 
     def __init__(self, iam_token: IAMToken, folder_id: str):
         self._iam_token = iam_token
         self._folder_id = folder_id
 
     def __call__(self, speech: bytes, language_code: str) -> str:
-        params = {'folderId': self._folder_id,
-                  'lang': language_code}
-        headers = {'Authorization': f'Bearer {self._iam_token.get()}'}
+        params = {"folderId": self._folder_id, "lang": language_code}
+        headers = {"Authorization": f"Bearer {self._iam_token.get()}"}
         try:
-            response = requests.post(self.__BASE_URL, params=params, data=speech, headers=headers)
+            response = requests.post(
+                self.__BASE_URL, params=params, data=speech, headers=headers
+            )
         except requests.exceptions.RequestException as e:
             raise SpeechRecognitionException
         if response.status_code != 200:
             raise SpeechRecognitionException(response.text)
-        return response.json()['result']
+        return response.json()["result"]
